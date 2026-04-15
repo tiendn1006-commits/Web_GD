1e27061f7ace67364f1317fe95cf845536858173
tiendn1006-commits <tiendn1006@gmail.com>
2026-04-15 14:31:56 +0700

    Updates

--- src/features/Home/components/index.js
+++
@@ -1,7 +0,0 @@
-export { Navbar } from './Navbar';
-export { Hero } from './Hero';
-export { About } from './About';
-export { CoursesList } from './CoursesList';
-export { Testimonials } from './Testimonials';
-export { Footer } from './Footer';
-export { RegistrationForm } from './RegistrationForm';
--- src/features/Home/routes.jsx
+++
@@ -1,8 +0,0 @@
-import { LandingPage } from './pages/LandingPage';
-
-export const homeRoutes = [
-  {
-    path: '/',
-    element: <LandingPage />
-  }
-];
